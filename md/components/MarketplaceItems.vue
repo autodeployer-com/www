80645eaff499e1e054b1cc22d51a786d701c6560
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-01-04 21:47:35 +0100

    Create MarketplaceItems.vue

---
+++ md/components/MarketplaceItems.vue
@@ -0,0 +1,60 @@
+napisz kompenent dla frameworku vue z sekcją style i script do wyświetlenia danych z pliku JSON zawierające tablicę z kolumnami: id, nazwa, opis, url
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nazwa</th>
+          <th>Opis</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.description }}</td>
+          <td>{{ item.url }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-component',
+  data () {
+    return {
+      items: []
+    }
+  },
+  created () {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(res => {
+        this.items = res
+      })
+  }
+}
+</script>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
