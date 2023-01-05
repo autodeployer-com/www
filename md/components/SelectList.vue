f845cb9f1477c8006187f2edb3dddb898cfd55c0
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-01-05 20:08:05 +0100

    Create SelectList.vue

---
+++ md/components/SelectList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <select v-model="selectedOption" @change="onChange">
+      <option v-for="option in options" :value="option.value" :key="option.value">
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [],
+      selectedOption: ''
+    }
+  },
+  created() {
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(data => this.options = data);
+  },
+  methods: {
+    onChange(e) {
+      this.selectedOption = e.target.value
+    }
+  }
+}
+</script>
